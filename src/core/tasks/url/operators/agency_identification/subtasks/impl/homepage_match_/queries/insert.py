cd4831569b66d47398873c8e41eee2ef8b77c55e
maxachis <maxachis@gmail.com>
2025-09-06 14:03:48 -0400

    Continue Draft

--- src/core/tasks/url/operators/agency_identification/subtasks/impl/homepage_match_/queries/insert.py
+++ src/core/tasks/url/operators/agency_identification/subtasks/impl/homepage_match_/queries/insert.py
@@ -2,3 +2,8 @@ from src.db.queries.base.builder import QueryBuilderBase
 
 
 class InsertHomepageMatchSubtaskEntriesQueryBuilder(QueryBuilderBase):
+    # TODO: Write Insert for Subtasks
+
+    # TODO: Write insert for Subtask entries
+
+    # TODO: Do URL link
