4ad3a2dfbfc1a7402630e1657e06763f04424ffb
Max Chis <maxachis@gmail.com>
2025-09-13 10:16:20 -0400

    Update imports

--- src/db/models/impl/task/core.py
+++ src/db/models/impl/task/core.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column
 from sqlalchemy.orm import relationship
 
 from src.db.enums import PGEnum, TaskType
+from src.db.models.impl.task.error import TaskError
 from src.db.models.mixins import UpdatedAtMixin
 from src.db.models.templates_.with_id import WithIDBase
 from src.db.models.types import batch_status_enum
@@ -23,5 +24,5 @@ class Task(UpdatedAtMixin, WithIDBase):
         secondary="link_task_urls",
         back_populates="tasks"
     )
-    error = relationship("TaskError", back_populates="task")
+    error = relationship(TaskError, back_populates="task")
     errored_urls = relationship("URLErrorInfo", back_populates="task")
