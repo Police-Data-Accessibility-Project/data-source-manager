c13f9cea005fc8da366410e12e6664da2d4d5e65
Max Chis <maxachis@gmail.com>
2025-09-25 16:50:09 -0400

    Create /search/agency endpoint with test

--- src/api/endpoints/search/agency/query.py
+++ src/api/endpoints/search/agency/query.py
@@ -1,23 +1,22 @@
-from typing import Any, Sequence
+from typing import Sequence
 
 from sqlalchemy import select, func, RowMapping
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.api.endpoints.search.agency.models.response import AgencySearchResponse
-from src.db import Location
+from src.db.helpers.session import session_helper as sh
 from src.db.models.impl.agency.sqlalchemy import Agency
 from src.db.models.impl.link.agency_location.sqlalchemy import LinkAgencyLocation
 from src.db.models.views.location_expanded import LocationExpandedView
 from src.db.queries.base.builder import QueryBuilderBase
 
-from src.db.helpers.session import session_helper as sh
 
 class SearchAgencyQueryBuilder(QueryBuilderBase):
 
     def __init__(
         self,
         location_id: int | None,
-        query: str
+        query: str | None
     ):
         super().__init__()
         self.location_id = location_id
@@ -45,12 +44,13 @@ class SearchAgencyQueryBuilder(QueryBuilderBase):
             query = query.where(
                 LocationExpandedView.id == self.location_id
             )
-        query = query.order_by(
-            func.similarity(
-                Agency.name,
-                self.query
-            ).desc()
-        )
+        if self.query is not None:
+            query = query.order_by(
+                func.similarity(
+                    Agency.name,
+                    self.query
+                ).desc()
+            )
 
         mappings: Sequence[RowMapping] = await sh.mappings(session, query)
 
--- src/api/endpoints/search/routes.py
+++ src/api/endpoints/search/routes.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, Query, Depends
+
+from fastapi import APIRouter, Query, Depends, HTTPException
+from starlette import status
 
 from src.api.dependencies import get_async_core
 from src.api.endpoints.search.agency.models.response import AgencySearchResponse
@@ -29,10 +31,19 @@ async def search_agency(
         description="The location id to search for",
         default=None
     ),
-    query: str = Query(description="The query to search for"),
+    query: str | None = Query(
+        description="The query to search for",
+        default=None
+    ),
     access_info: AccessInfo = Depends(get_access_info),
     async_core: AsyncCore = Depends(get_async_core),
 ) -> list[AgencySearchResponse]:
+    if query is None and location_id is None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="At least one of query or location_id must be provided"
+        )
+
     return await async_core.adb_client.run_query_builder(
         SearchAgencyQueryBuilder(
             location_id=location_id,
