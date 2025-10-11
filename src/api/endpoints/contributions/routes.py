24edf04dfad71cdc0a678177af539d311e26ef09
Max Chis <maxachis@gmail.com>
2025-10-11 18:21:39 -0400

    Add description for `get_user_contributions`

--- src/api/endpoints/contributions/routes.py
+++ src/api/endpoints/contributions/routes.py
@@ -19,6 +19,7 @@ async def get_leaderboard(
     core: AsyncCore = Depends(get_async_core),
     access_info: AccessInfo = Depends(get_access_info)
 ) -> ContributionsLeaderboardResponse:
+    """Returns the leaderboard of user contributions."""
     return await core.adb_client.run_query_builder(
         GetContributionsLeaderboardQueryBuilder()
     )
@@ -28,6 +29,18 @@ async def get_user_contributions(
     core: AsyncCore = Depends(get_async_core),
     access_info: AccessInfo = Depends(get_access_info)
 ) -> ContributionsUserResponse:
+    """Get contributions for the user and how often their annotations agreed with the final validation of URLs.
+
+    Agreement for each is based the number of the user's correct annotations for that URL attribute
+    divided by their total number of annotations for that URL attribute.
+
+    "Correct" in this case means the user's annotation value for that URL attribute
+    aligned with the final validated value for that attribute.
+
+    In the case of attributes with multiple validated values, such as agency ID,
+    agreement is determined if the user's suggested value aligns with any of the final validated values.
+    """
+
     return await core.adb_client.run_query_builder(
         GetUserContributionsQueryBuilder(access_info.user_id)
     )
