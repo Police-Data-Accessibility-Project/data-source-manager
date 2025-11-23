1816c4e23a663129644a50c38071bdbd0a0f1193
Max Chis <maxachis@gmail.com>
2025-11-23 09:53:40 -0500

    Remove dependencies from apply_migrations

--- apply_migrations.py
+++ apply_migrations.py
@@ -1,15 +1,19 @@
 from alembic import command
 from alembic.config import Config
 
-from src.db.helpers.connect import get_postgres_connection_string
+from src.util.helper_functions import get_from_env
 
 
 def apply_migrations():
     print("Applying migrations...")
     alembic_config = Config("alembic.ini")
+    connection_string = (
+        f"postgresql://{get_from_env('POSTGRES_USER')}:{get_from_env('POSTGRES_PASSWORD')}" +
+        f"@{get_from_env('POSTGRES_HOST')}:{get_from_env('POSTGRES_PORT')}/{get_from_env('POSTGRES_DB')}")
+
     alembic_config.set_main_option(
         "sqlalchemy.url",
-        get_postgres_connection_string()
+        connection_string
     )
     command.upgrade(alembic_config, "head")
     print("Migrations applied.")
