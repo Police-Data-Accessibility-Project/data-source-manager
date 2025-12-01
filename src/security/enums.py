3ed9106b0d840072694233e739002866c69e4d76
Max Chis <maxachis@gmail.com>
2025-12-01 15:21:04 -0500

    Update source collector permission

--- src/security/enums.py
+++ src/security/enums.py
@@ -2,5 +2,5 @@ from enum import Enum
 
 
 class Permissions(Enum):
-    SOURCE_COLLECTOR = "source_collector"
+    SOURCE_COLLECTOR = "access_source_collector"
     SOURCE_COLLECTOR_FINAL_REVIEW = "source_collector_final_review"
