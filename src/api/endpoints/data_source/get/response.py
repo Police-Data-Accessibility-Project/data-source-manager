b0ab4d40480b3f205154b39f0bfb78ec4bcd69e3
Max Chis <maxachis@gmail.com>
2026-02-26 20:09:37 -0500

    Remove agency_described_not_in_database from data sources metadata

--- src/api/endpoints/data_source/get/response.py
+++ src/api/endpoints/data_source/get/response.py
@@ -29,7 +29,6 @@ class DataSourceGetResponse(BaseModel):
     agency_supplied: bool | None = None
     agency_originated: bool | None = None
     agency_aggregation: AgencyAggregationEnum | None = None
-    agency_described_not_in_database: str | None = None
     update_method: UpdateMethodEnum | None = None
     readme_url: str | None = None
     originating_entity: str | None = None
